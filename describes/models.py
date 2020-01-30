1189cda3efcdf6a9d327ee6056226718d99705a1
llsonKimm <lsoncheruiyot@gmail.com>
2020-01-30 16:52:11 +0300

    First heroku commit

--- describes/models.py
+++ describes/models.py
@@ -7,5 +7,3 @@ from django.db.models import FilePathField
 class Describes(models.Model) :
     title = models.CharField(max_length=100)
     description = models.TextField()
-    technology = models.CharField(max_length=20)
-    image = FilePathField(path='/img')
